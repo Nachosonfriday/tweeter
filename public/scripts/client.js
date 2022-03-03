477d91150d4ce8db78ec40607692e7bbd785d247
Manuel Zuniga <zuniga@ualberta.ca>
2022-03-02 17:43:46 -0700

    created a generate tweet function that appends new tweets to the tweet container. right now it is using hard coded data. Will recitfy in next commit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,44 +6,60 @@
 
 // Generates a tweet component
 $(document).ready(function() {
-  console.log("hey there!")
-
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      let newTweet = createTweetElement(tweet)
+      $('.tweet-container').append(newTweet);  
+    }   
   }
 
   const createTweetElement = (tweetObj) => {
     return (`
-    <article class="tweet">
-    <header class="tweet-header">
-      <p><i class="fa-solid fa-face-grin-hearts"></i>   ${tweetObj.user.name}</p><p>${tweetObj.user.handle}</p>
-    </header>
-       
+      <article class="tweet">
+        <header class="tweet-header">
+          <p><i class="fa-solid fa-face-grin-hearts"></i>   ${tweetObj.user.name}</p>
+          <p>${tweetObj.user.handle}</p>
+        </header>
         <p class="tweet-paragraph">${tweetObj.content.text}</p>
-      
-    <footer class="tweet-footer">
-      <p>${tweetObj.created_at}</p>
-      <div class="icons">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
-  </article> `)
+        <footer class="tweet-footer">
+          <p>${timeago.format(tweetObj.created_at)}</p>
+          <div class="icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article> `
+    )
   }
-  
-  const $tweet = createTweetElement(tweetData);
 
-  console.log($tweet);
-  $('.tweet-container').append($tweet);
+  renderTweets(data);
 })
 
 
